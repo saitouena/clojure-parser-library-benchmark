9b4e6d9903b067506dd9a1be9adc62b5e61f4a7b
saitouena <saitouena@protonmail.com>
2019-09-28 17:43:40 +0900

    instaparse: add true, false, null, string, object

--- src/clojure_parser_library_benchmark/instaparse.clj
+++ src/clojure_parser_library_benchmark/instaparse.clj
@@ -10,32 +10,111 @@
 
 (def json
   (insta/parser
-   "number = integer
+   "element = ws value ws
+    value = object
+          | number
+          | true
+          | false
+          | null
+          | string
+    object = <'{'> ws <'}'>
+           | <'{'> member (<','> member)* <'}'>
+    member = ws string ws <':'> element
+    <ws> = w+
+    <w> = <''> | <'\t'> | <' '> | <'\r'> | <'\n'>
+    number = integer
     integer = posint
             | negint
     posint = digit | onenine digit+
-    negint = '-' digit | '-' onenine digit+
-    digit = '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
-    onenine = '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'"))
+    negint = <'-'> digit | <'-'> onenine digit+
+    <digit> = '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
+    <onenine> = '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
+    true = 'true'
+    false = 'false'
+    null = 'null'
+    string = <'\"'> character+ <'\"'>
+    <character> = #'[a-zA-Z]'"))
 
-(json "1234")
+;; TODO: character
+
+(defn transform-return-itself [& [x]] x)
 
 (defn transform-posint
   [& children]
-  (reduce (fn [x acc] (-> (* 10 acc) (+ x)))
-          0
-          children))
+  (read-string (apply str children)))
 
 (defn transform-negint
   [& children]
-  (- (transform-posint (rest children))))
+  (- (apply transform-posint children)))
 
 (def transform-digit (comp read-string str))
 
 (def transform-onenine (comp read-string str))
 
-(->> (json "1234")
-     (insta/transform {:posint transform-posint
-                       :negint transform-negint
-                       :digit transform-digit
-                       :onenine transform-onenine}))
+(def transform-true (comp read-string str))
+
+(def transform-false (comp read-string str))
+
+(defn transform-null [& _] nil)
+
+(defn transform-character [& [c]] c)
+
+(def transform-string str)
+
+(defn transform-integer [& [n]] n)
+
+(defn transform-number [& [n]] n)
+
+(defn transform-member [& [key val]] {key val})
+
+(defn transform-object [& members] (apply merge members))
+
+
+(def how-to-transform
+  {:element transform-return-itself
+   :value transform-return-itself
+   :object transform-object
+   :member transform-member
+   :number transform-number
+   :integer transform-integer
+   :posint transform-posint
+   :negint transform-negint
+   :digit transform-digit
+   :onenine transform-onenine
+   :true transform-true
+   :false transform-false
+   :null transform-null
+   :character transform-character
+   :string transform-string
+   })
+
+(def transform-json
+  (partial insta/transform how-to-transform))
+
+(def parse (comp transform-json json))
+
+(json "1234")
+
+(json "-1234")
+
+(parse "1234")
+
+(parse "-1234")
+
+(parse "true")
+
+(parse "false")
+
+(parse "null")
+
+(parse "\"hoge\"")
+
+(json "{}")
+(json "{          }")
+(json "{     \"hoge\" : 1, \"fuga\" : -10}")
+
+(parse "{     \"hoge\" : 1, \"fuga\" : -10}")
+
+(json "        " :start :ws)
+
+"\u0021" ;; unicode
